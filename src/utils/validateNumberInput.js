06d791d9056481d7df31291f1bd7df4de0b8f309
ytka <ytka@Ytkas-MacBook-Pro.local>
2023-11-28 19:14:26 +0200

    fix input validation bug

--- src/utils/validateNumberInput.js
+++ src/utils/validateNumberInput.js
@@ -1,5 +1,9 @@
 const validateNumberInput = (newValue) => {
-  if (newValue.match(`(^$)|(^[1-9]([0-9]*)(\\.?[0-9]{0,2})$)`)) {
+  if (
+    newValue.match(
+      `(^0(\\.[0-9]{0,2})?$)|(^$)|(^[1-9]([0-9]*)(\\.?[0-9]{0,2})$)`
+    )
+  ) {
     return true;
   }
   return false;
